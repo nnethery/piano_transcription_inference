0206e36697d1587b200373a45853a47f94a356bb
Noah Nethery <nethery.n@gmail.com>
2023-05-18 13:42:18 -0400

    Add torch req

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['matplotlib', 'mido', 'librosa', 'torchlibrosa', 'soundfile'],
+    install_requires=['matplotlib', 'mido', 'librosa', 'torchlibrosa', 'soundfile', 'torch'],
     python_requires='>=3.6',
 )
